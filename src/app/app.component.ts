f566cbced330774700001027b21ed6ff24b09a9e
Bryan Rodriguez <bryanrodriguez@Bryans-iMac.local>
2018-09-25 13:12:40 -0500

    geolocalzacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Platzi Square';
+
+  lugares:any = [
+    {active: true, nombre:'Panaderia Bendito el pan'},
+    {active: true, nombre:'Restaurante la terraza del gordo'},
+    {active: true, nombre:'Armando Chicharron'},
+    {active: false, nombre:'Bendito el pan'},
+    {active: true, nombre:'la terraza del gordo'},
+    {active: true, nombre:'Armando Chicharron'}
+  ];
+
+  personas:any = [
+      {nombre:'Juan Perez',edad: 18},
+      {nombre:'Carlos Gonzalez',edad: 23},
+      {nombre:'Marcos Lugo',edad: 38},
+      {nombre:'Alvaro Gomez',edad: 13},
+      {nombre:'Maria Lopez',edad: 18}
+    ]
+
+  lat: number = 4.6972091;
+  lng: number = -74.0367337;
+
+  constructor(){
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCVQZK8skXBfmYz135t5ye2tZhMrbymyA4'
+    })
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
